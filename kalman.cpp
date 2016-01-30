023f65e1dc4a14c62223a8b7b214fb7c44124cc8
MicrocontrollersAndMore <cdahms123@gmail.com>
2016-01-30 09:01:45 -0500

    1

--- kalman.cpp
+++ kalman.cpp
@@ -15,43 +15,38 @@ const cv::Scalar SCALAR_BLACK = cv::Scalar(0.0, 0.0, 0.0);
 const cv::Scalar SCALAR_BLUE = cv::Scalar(255.0, 0.0, 0.0);
 const cv::Scalar SCALAR_GREEN = cv::Scalar(0.0, 200.0, 0.0);
 const cv::Scalar SCALAR_RED = cv::Scalar(0.0, 0.0, 255.0);
-const cv::Scalar SCALAR_TEAL = cv::Scalar(255.0, 255.0, 0.0);
-const cv::Scalar SCALAR_BROWN = cv::Scalar(0.0, 155.0, 255.0);
 
 cv::Point ptActualMousePosition(0, 0);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) {
 
-    cv::KalmanFilter kalmanFilter(4, 2, 0);
+    cv::KalmanFilter kalmanFilter(4, 2, 0);                             // instantiate Kalman Filter
 
-    float fltTransitionMatrixValues[4][4] = { { 1, 0, 1, 0 },                   // research this !!!!!!!!!!!!!!!!!!!
+    float fltTransitionMatrixValues[4][4] = { { 1, 0, 1, 0 },           // declare an array of floats to feed into Kalman Filter Transition Matrix, also known as State Transition Model
                                               { 0, 1, 0, 1 },
                                               { 0, 0, 1, 0 },
                                               { 0, 0, 0, 1 } };
-
-    kalmanFilter.transitionMatrix = cv::Mat(4, 4, CV_32F, fltTransitionMatrixValues);
-
-    /*
-    kalmanFilter.statePre.at<float>(0) = 0;
-    kalmanFilter.statePre.at<float>(1) = 0;
-    kalmanFilter.statePre.at<float>(2) = 0;
-    kalmanFilter.statePre.at<float>(3) = 0;
-    */
-                                                                           // research these !!!!!!!!!!!!!!!!!!!
-    cv::setIdentity(kalmanFilter.measurementMatrix);                                //    
-    cv::setIdentity(kalmanFilter.processNoiseCov, cv::Scalar::all(1));           // was 1e-4
-    cv::setIdentity(kalmanFilter.measurementNoiseCov, cv::Scalar::all(1));         // was 10
-    cv::setIdentity(kalmanFilter.errorCovPost, cv::Scalar::all(1));               // was 0.1
     
-    cv::Mat image(600, 800, CV_8UC3, cv::Scalar::all(0));
+    kalmanFilter.transitionMatrix = cv::Mat(4, 4, CV_32F, fltTransitionMatrixValues);       // set Transition Matrix
 
-    std::vector<cv::Point> predictedMousePositions;
+    float fltMeasurementMatrixValues[2][4] = { { 1, 0, 0, 0 },          // declare an array of floats to feed into Kalman Filter Measurement Matrix, also known as Measurement Model
+                                               { 0, 1, 0, 0 } };
+
+    kalmanFilter.measurementMatrix = cv::Mat(2, 4, CV_32F, fltMeasurementMatrixValues);     // set Measurement Matrix
+
+    cv::setIdentity(kalmanFilter.processNoiseCov, cv::Scalar::all(0.0001));           // default is 1, for smoothing try 0.0001
+    cv::setIdentity(kalmanFilter.measurementNoiseCov, cv::Scalar::all(10));         // default is 1, for smoothing try 10
+    cv::setIdentity(kalmanFilter.errorCovPost, cv::Scalar::all(0.1));               // default is 0, for smoothing try 0.1
+
+    cv::Mat imgBlank(700, 900, CV_8UC3, cv::Scalar::all(0));            // declare a blank image for moving the mouse over
+
+    std::vector<cv::Point> predictedMousePositions;                 // declare 3 vectors for predicted, actual, and corrected positions
     std::vector<cv::Point> actualMousePositions;
     std::vector<cv::Point> correctedMousePositions;
 
-    cv::namedWindow("image");
-    cv::setMouseCallback("image", mouseMoveCallback);
+    cv::namedWindow("imgBlank");                                // declare window
+    cv::setMouseCallback("imgBlank", mouseMoveCallback);        // 
 
     while (true) {
         cv::Mat matPredicted = kalmanFilter.predict();
@@ -73,27 +68,27 @@ int main(void) {
 
             // predicted, actual, and corrected are all now calculated, time to draw stuff
 
-        drawCross(image, ptPredicted, SCALAR_BLUE);
-        drawCross(image, ptActualMousePosition, SCALAR_WHITE);
-        drawCross(image, ptCorrected, SCALAR_GREEN);
+        drawCross(imgBlank, ptPredicted, SCALAR_BLUE);                      // draw a cross at the most recent predicted, actual, and corrected positions
+        drawCross(imgBlank, ptActualMousePosition, SCALAR_WHITE);
+        drawCross(imgBlank, ptCorrected, SCALAR_GREEN);
         
-        for (int i = 0; i < predictedMousePositions.size() - 1; i++) {
-            cv::line(image, predictedMousePositions[i], predictedMousePositions[i + 1], SCALAR_BLUE, 1);
+        for (int i = 0; i < predictedMousePositions.size() - 1; i++) {                  // draw each predicted point in blue
+            cv::line(imgBlank, predictedMousePositions[i], predictedMousePositions[i + 1], SCALAR_BLUE, 1);
         }
 
-        for (int i = 0; i < actualMousePositions.size() - 1; i++) {
-            cv::line(image, actualMousePositions[i], actualMousePositions[i + 1], SCALAR_WHITE, 1);
+        for (int i = 0; i < actualMousePositions.size() - 1; i++) {                     // draw each actual point in white
+            cv::line(imgBlank, actualMousePositions[i], actualMousePositions[i + 1], SCALAR_WHITE, 1);
         }
 
-        for (int i = 0; i < correctedMousePositions.size() - 1; i++) {
-            cv::line(image, correctedMousePositions[i], correctedMousePositions[i + 1], SCALAR_GREEN, 1);
+        for (int i = 0; i < correctedMousePositions.size() - 1; i++) {                  // draw each corrected point in green
+            cv::line(imgBlank, correctedMousePositions[i], correctedMousePositions[i + 1], SCALAR_GREEN, 1);
         }
 
-        cv::imshow("image", image);         // show image
+        cv::imshow("imgBlank", imgBlank);         // show the image
         
-        cv::waitKey(10);                    // pause for a moment to get operating system to redraw the image
+        cv::waitKey(10);                    // pause for a moment to get operating system to redraw the imgBlank
 
-        image = cv::Scalar::all(0);         // blank the image for next time around
+        imgBlank = cv::Scalar::all(0);         // blank the imgBlank for next time around
     }
 
     return 0;
